293ed168e113eacacf893db13dc6bc02fc85e931
Starostin Sergey <Vlillen@yandex.ru>
2023-06-09 14:49:14 +0400

    Added Task3

--- HomeWork/src/Main.java
+++ HomeWork/src/Main.java
@@ -8,8 +8,26 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        //SumFromOneToN();
-        PrimeNumbers();
+        
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("1) Вычислить сумма чисел от 1 до n");
+        System.out.println("2) Вывести все простые числа от 1 до 1000");
+        System.out.println("3) Реализовать простой калькулятор");
+        System.out.println("Ввыедите номер задачи: ");
+        int tasks = in.nextInt();
+        
+        switch(tasks){
+            case 1:
+                SumFromOneToN();
+                break;
+            case 2:
+                PrimeNumbers();
+                break;
+            case 3:
+                Calculator();
+                break;
+        }
     }
 
     private static void SumFromOneToN() {
@@ -38,5 +56,34 @@ public class Main {
             }
         }
     }
+    private static void Calculator() {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите первое число: ");
+        double num1 = scanner.nextDouble();
+        System.out.print("Введите оператор (+, -, *, /): ");
+        char action = scanner.next().charAt(0);
+        System.out.print("Введите второе число: ");
+        double num2 = scanner.nextDouble();
+        double result = 0;
+
+        switch (action) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                result = num1 / num2;
+                break;
+            default:
+                System.out.println("Ошибка ввода");
+                return;
+        }
+        System.out.println(num1 + " " + action + " " + num2 + " = " + result);
+    }
 }
