6050c797f60da90c97a971b75ec70199f31bdd64
Starostin Sergey <Vlillen@yandex.ru>
2023-06-09 13:05:37 +0400

    Added Task2

--- HomeWork/src/Main.java
+++ HomeWork/src/Main.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        SumFromOneToN();
-
+        //SumFromOneToN();
+        PrimeNumbers();
     }
 
     private static void SumFromOneToN() {
@@ -23,5 +23,20 @@ public class Main {
         System.out.println("Сумма чисел от 1 до " + n + " равна " + sum);
     }
 
-}
+    private static void PrimeNumbers() {
+        System.out.println("Простые числа от 1 до 1000: ");
+        for (int i = 1; i <= 1000; i++) {
+            boolean isPrime = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.print(i + ", ");
+            }
+        }
+    }
 
+}
