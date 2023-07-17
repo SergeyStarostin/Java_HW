effcc29c069d7f4cfeaae289b8f03dfdfb36c147
Y127908 <Sergey.starostin@vaz.ru>
2023-07-17 16:27:42 +0400

    Added Task_5

---
+++ OOP_HomeWork6/src/Task_4/Rectangle.java
@@ -0,0 +1,7 @@
+package Task_4;
+
+public class Rectangle extends Form {
+    public Rectangle(int width, int height) {
+        super(width, height);
+    }
+}
---
+++ OOP_HomeWork6/src/Task_5/Car.java
@@ -0,0 +1,13 @@
+package Task_5;
+
+public class Car {
+    private Engine engine;
+
+    public Car(Engine engine) {
+        this.engine = engine;
+    }
+
+    public void start() {
+        this.engine.start();
+    }
+}
